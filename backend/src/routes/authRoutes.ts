e5936fcc99870800d99d94ba6d0ef37e481c54fb
preyum <mfpriyam@gmail.com>
2024-09-05 13:46:49 +0530

    add: definitive status codes and messages for response, quality of life changes

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -6,7 +6,6 @@ import { createUser } from "../Controllers/userController";
 import Authenticate from "../Middleware/Authenticate";
 
 import {
-	hash,
 	registerValidation,
 	loginValidation,
 } from "../Controllers/validation";
@@ -14,9 +13,6 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import { v4 as uuidv4 } from "uuid";
 import nodemailer, { Transporter } from "nodemailer";
-
-import e from "express";
-import { log } from "console";
 
 // import * as dotenv from "dotenv";
 // dotenv.config();
@@ -28,11 +24,11 @@ const secret = process.env.JWT_PASS;
 router.post("/register", async (req: Request, res: Response) => {
 	// validate data before creating user
 	const { error, value } = registerValidation(req.body);
-	if (error) return res.send(error.details[0].message);
+	if (error) return res.status(400).send({ message: "Validation failed", error: error.details[0].message });
 
 	// check if user already exists
 	const emailExists = await UserModel.findOne({ email: req.body.email });
-	if (emailExists) return res.send("Email already exists");
+	if (emailExists) return res.status(409).send({ message: "Email already exists" });
 
 	// Hash password
 	let hashPassword: string = "";
@@ -69,53 +65,53 @@ router.post("/register", async (req: Request, res: Response) => {
 		createdAt: new Date(),
 	};
 	await createUser(newUser)
-		.then(() => res.send("user created"))
-		.catch((error) => console.log(error));
+		.then(() => res.status(201).send({ message: "user created" }))
+		.catch((err) => res.status(500).send({
+			message: "An error occurred while saving the user",
+			error: err
+		})
+		);
 });
 
 // login
 router.post("/login", async (req: Request, res: Response) => {
 	// validate data before creating user
 	const { error, value } = loginValidation(req.body);
-	if (error) return res.send(error.details[0].message);
+	if (error) return res.status(400).send({ message: "Validation failed", error: error.details[0].message });
 
 	// check if user exists
 	const user = await UserModel.findOne({ email: req.body.email });
 	if (!user)
-		return res.send({
-			message: "Email does not exist",
-		});
+		return res.status(401)
+			.send({
+				message: "Invalid credentials"
+			});
 
 	// check password
 	const validPass = await bcrypt.compare(req.body.password, user.password);
 	if (!validPass)
-		return res.status(400).send({
-			message: "Invalid Password",
+		return res.status(401).send({
+			message: "Invalid credentials"
 		});
 
 	if (!secret) {
 		console.log("Not a secret");
 	} else {
 		// Create JWT token
-		const token = jwt.sign(
-			{
-				_id: user._id,
-			},
-			secret,
-		);
-		res.header("authToken", token).send(token);
+		const token: String = jwt.sign(
+			{ _id: user._id }, secret);
+		res.status(200)
+			.cookie("authToken", token)
+			.send({ message: "Login sucessful" });
 	}
-	// res.send({
-	//   message: "Logged in"
-	// })
 });
 let TOKEN: string = "";
 router.post("/forgot-password", async (req, res) => {
 	const email = req.body.email;
-	const user: any = await UserModel.findOne({ email: email });
+	const user: User | null = await UserModel.findOne({ email: email });
 	if (!user)
-		return res.status(404).send({
-			msg: "User does not exist",
+		return res.status(401).send({
+			message: "Invalid credentials"
 		});
 
 	if (!secret) {
@@ -124,7 +120,7 @@ router.post("/forgot-password", async (req, res) => {
 		// Create JWT token
 		TOKEN = jwt.sign(
 			{
-				_id: user._id,
+				_id: user.id,
 			},
 			secret,
 		);
@@ -144,7 +140,7 @@ router.post("/forgot-password", async (req, res) => {
 		from: "skyhighyes@gmail.com",
 		to: user.email,
 		subject: "Reset your password",
-		text: `http://localhost:${process.env.PORT}/auth/reset-password/${user._id}/${TOKEN}`,
+		text: `http://localhost:${process.env.PORT}/auth/reset-password/${user.id}/${TOKEN}`,
 	};
 
 	// Send the email
@@ -152,8 +148,8 @@ router.post("/forgot-password", async (req, res) => {
 		if (error) {
 			console.log(error);
 		} else {
-			return res.send({
-				msg: "Email sent",
+			return res.status(200).send({
+				message: "Email sent",
 			});
 		}
 	});
@@ -163,11 +159,11 @@ router.post("/reset-password/:id/:token", (req, res) => {
 	const id = req.params.id;
 	const token = req.params.token;
 
-	if (!secret) return res.send({ msg: "Error with token" });
+	if (!secret) return res.status(401).send({ message: "Invalid or expired token" });
 	else {
 		jwt.verify(token, secret, async (err, decoded) => {
 			if (err) {
-				return res.json({ msg: "Token error" });
+				return res.status(401).send({ message: "Invalid or expired token" });
 			} else {
 				// Hash password
 				let hashPassword: string = "";
@@ -194,12 +190,12 @@ router.post("/reset-password/:id/:token", (req, res) => {
 				}
 				try {
 					const user = await UserModel.findById(id);
-					if (!user) return res.send({ msg: "user not found" });
+					if (!user) return res.status(401).send({ message: "Invalid credentials" });
 					user.password = hashPassword;
 					await user.save();
-					return res.send({ msg: "Password updated successfully" });
+					return res.status(200).send({ message: "Password updated successfully" });
 				} catch (e) {
-					return res.send({ msg: e });
+					return res.status(500).send({ message: "An error occurred while saving the user", error: e });
 				}
 			}
 		});
@@ -224,8 +220,8 @@ router.get("/logout", (req, res) => {
 
 // logout route
 router.get("/logout", (req, res) => {
-  res.clearCookie("authToken", { path: "/" }); 
-  res.status(200).send("User logged out successfully"); 
+	res.clearCookie("authToken", { path: "/" });
+	res.status(200).send("User logged out successfully");
 });
 
 export default router;
