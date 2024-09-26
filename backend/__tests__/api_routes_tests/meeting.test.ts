861e721e45e0d0b273c8d78df5d0b26e04c8ad4b
Soumyadip Bhattacharjya <101652501+SoumyaCO@users.noreply.github.com>
2024-09-26 10:07:05 +0530

    test & ci: test cases for meetings and initial CI(test) added

--- backend/__tests__/api_routes_tests/meeting.test.ts
+++ backend/__tests__/api_routes_tests/meeting.test.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import request from "supertest";
 
 import { httpServer } from "../../src/index";
-import { randomUser } from "../test_utils/random";
+import { randomUser, randomUserName } from "../test_utils/random";
 
 /* Connecting to the databas before each test. */
 beforeEach(async () => {
@@ -31,20 +31,34 @@ afterEach(async () => {
  */
 
 describe("Create, Update, Get and Delete of Meeting data", () => {
+	const meetingID = randomUserName(20);
 	it("should create a random user", async () => {
-		const res = await request(httpServer).post("/meeting/createMeeting").send();
+		const res = await request(httpServer).post("/meeting/createMeeting").send({
+			ownerUsername: "username",
+			title: "New Meeting",
+			date: "2024-10-24T06:01:29.133Z",
+			meetingID: meetingID,
+		});
+		expect(res.statusCode).toBe(201);
+		expect(res.body).toBeDefined();
 	});
 
 	it("should update the user", async () => {
 		const res = await request(httpServer)
 			.put(`/meeting/updateMeeting/${meetingID}`)
-			.send({});
+			.send({
+				title: "Updated Meeting",
+				date: "2009-11-11T06:01:29.133Z",
+			});
 		expect(res.statusCode).toBe(201);
-		expect(res.body.data).toBeDefined();
 	});
 
 	it("Should delete the data successfully", async () => {
-		const res = await request(httpServer).delete(`/meeting/${meetingID}`);
+		const res = await request(httpServer).delete(
+			`/meeting/deleteMeeting/${meetingID}`,
+		);
+		// FIXME: Should return 200 IG
+		expect(res.statusCode).toBe(201);
 	});
 
 	// cookie based auth will happen in the backend
