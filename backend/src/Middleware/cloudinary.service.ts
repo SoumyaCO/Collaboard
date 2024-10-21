60a611391c86c69531998afbf04fb3e4233e749a
preyum <mfpriyam@gmail.com>
2024-10-21 23:07:28 +0530

    feat: added middlewares for file upload func

---
+++ backend/src/Middleware/cloudinary.service.ts
@@ -0,0 +1,38 @@
+import { v2 as cloudinary, UploadApiResponse } from 'cloudinary';
+import fs from 'fs';
+
+const uploadOnCloudinary = async (localFilePath: string): Promise<string> => {
+  // cloudinary config
+  cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME as string,
+    api_key: process.env.CLOUD_API_KEY as string,
+    api_secret: process.env.CLOUD_API_SECRET as string,
+  });
+
+  try {
+    // file upload with resizing
+    const uploadResult: UploadApiResponse = await cloudinary.uploader
+    .upload(localFilePath, {
+      resource_type: 'image',
+      transformation: {
+        width: 64,  // Resize to a width of 64px
+        height: 64, // Resize to a height of 64px
+        crop: 'limit', // Crop the image to fit within the 64x64 size
+      },
+      public_id: undefined,
+    });
+
+    // Delete the local file after upload
+    fs.unlinkSync(localFilePath);
+
+    // Return the secure URL
+    return uploadResult.secure_url;
+  } catch (error) {
+    // Delete the file in case of an error
+    fs.unlinkSync(localFilePath);
+    console.error('Error while uploading to Cloudinary:', error);
+    throw error;
+  }
+};
+
+export { uploadOnCloudinary };
---
+++ backend/src/Middleware/multer.middleware.ts
@@ -0,0 +1,16 @@
+import multer, { StorageEngine } from 'multer';
+import { Request } from 'express';
+
+const storage: StorageEngine = multer.diskStorage({
+  destination: function (req: Request, file: Express.Multer.File, cb: (error: Error | null, destination: string) => void) {
+    cb(null, '/uploads');
+  },
+  filename: function (req: Request, file: Express.Multer.File, cb: (error: Error | null, filename: string) => void) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    cb(null, file.fieldname + '-' + uniqueSuffix);
+  }
+});
+
+export const upload = multer({ storage: storage });
+
+
